e59373519eea72f3e560e65c11cf530ce63edfdf
Carlo <138653607+JustbeV@users.noreply.github.com>
2026-05-25 22:57:44 +0800

    Update element.sh

--- element.sh
+++ element.sh
@@ -1,39 +1,31 @@
 #!/bin/bash
+#1
+#2
+#3
+#4
+#5
+#7
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-if [[ -z $1 ]]; then
+if [[ -z $1 ]]
+then
   echo "Please provide an element as an argument."
-  exit 0
+else
+
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    QUERY=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
+  else
+    QUERY=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1' OR name='$1'")
+  fi
+
+  if [[ -z $QUERY ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$QUERY"
+
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
+  fi
 fi
-
-INPUT=$1
-
-# query: match atomic_number OR symbol OR name
-RESULT=$($PSQL "
-SELECT
-  e.atomic_number,
-  e.name,
-  e.symbol,
-  p.atomic_mass,
-  p.melting_point_celsius,
-  p.boiling_point_celsius,
-  t.type
-FROM elements e
-JOIN properties p USING(atomic_number)
-JOIN types t USING(type_id)
-WHERE
-  e.atomic_number::text = '$INPUT'
-  OR e.symbol ILIKE '$INPUT'
-  OR e.name ILIKE '$INPUT';
-")
-
-if [[ -z $RESULT ]]; then
-  echo "I could not find that element in the database."
-  exit 0
-fi
-
-echo "$RESULT" | while IFS="|" read ATOMIC NAME SYMBOL MASS MELTING BOILING TYPE
-do
-  echo "The element with atomic number $ATOMIC is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
-done
